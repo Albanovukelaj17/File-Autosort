fe6f08cc1daaa795b81f1d496ec7893136b5c815
Albano <albigoal@outlook.com>
2024-09-23 15:34:27 +0200

    categories

---
+++ main.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+let file_categories :[String: String] = [
+    "jpg": "Pictures",
+    "jpeg": "Pictures",
+    "png": "Pictures",
+    "pdf": "Documents",
+    "docx": "Documents",
+    "txt": "Documents",
+    "mp4": "Videos",
+    "mp3": "Music"
+]

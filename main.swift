3816ff96d740996b9c339039ed5545dd7e7bff22
Albano <albigoal@outlook.com>
2024-09-24 12:49:06 +0200

    move 1.0

--- main.swift
+++ main.swift
@@ -10,3 +10,19 @@ let file_categories :[String: String] = [
     "mp4": "Videos",
     "mp3": "Music"
 ]
+func organizeFiles(in directory: String) {
+    let fileManager = FileManager.default
+    let sourceDir = URL(fileURLWithPath: directory)
+    let destinationBaseDir = URL(fileURLWithPath: NSHomeDirectory())
+    
+    // Get all files in the directory
+    do {
+        let files = try fileManager.contentsOfDirectory(atPath: directory)
+        
+        for file in files {
+            moveFileToCategory(file: file, sourceDir: sourceDir, destinationBaseDir: destinationBaseDir)
+        }
+    } catch {
+        print("Error accessing directory: \(error.localizedDescription)")
+    }
+}
